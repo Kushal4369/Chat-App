89f558b4d6a6c8667625f6ec19ba3445a60a4a82
Kushal4369 <143159808+Kushal4369@users.noreply.github.com>
2024-07-02 17:00:23 +0530

    st part - backend

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -33,7 +33,8 @@ export const sendMessage= async (req,res)=> {
 
         res.status(201).json(newMessage)
     } catch (error) {
-        
+        console.log("Error in sendMessage controller:", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 }
 
